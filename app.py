c02fdcd7344f1df3d905268d1587d5ab51657315
Nikhil Senthil Kumar <68553237+NSK0904@users.noreply.github.com>
2025-08-23 09:06:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def product(batch_id):
     return render_template("product.html", product=product_info, traceability=product_info["traceability"])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(debug=True, host="0.0.0.0", port=port)
